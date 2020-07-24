116791d644184b8519e8b327ae5642a3124465ad
Jay Kim <jaykim92@gmail.com>
2020-07-24 00:55:37 -0700

    deleted nav component and combined with header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function Header() {
-
+    return(
+        <header>
+            <nav className='navbar'>
+                <button>Home</button>
+                <button>Portfolio</button>
+                <button>Resume</button>
+                <button>Contact</button>
+            </nav>            
+        </header>
+    );
 };
 
 export default Header;
