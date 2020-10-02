30561b649d3d96568379e4dd89b11d2422e3b13f
Jay Kim <jaykim92@gmail.com>
2020-10-02 00:30:45 -0700

    starting the rework of projects page

--- src/components/project.js
+++ src/components/project.js
@@ -2,50 +2,63 @@
 import React from "react";
 
 // assets
-import inorout from "../assets/inorout.png";
-import weather from "../assets/weather-dashboard.png";
-import dayplanner from "../assets/day-planner.png";
+// import inorout from "../assets/inorout.png";
+// import weather from "../assets/weather-dashboard.png";
+// import dayplanner from "../assets/day-planner.png";
 
-const imgCMS = {
-  inorout,
-  "weather-dashboard": weather,
-  "day-planner": dayplanner
-};
+// const imgCMS = {
+//   inorout,
+//   "weather-dashboard": weather,
+//   "day-planner": dayplanner
+// };
 
 function Project({ data }) {
   return (
-    <div
-      className=" row full-height"
-      data-aos="fade-up"
-      data-aos-duration="700"
-      id="projects"
-    >
-      <div className="col-sm-12 custom-margin-top">
-        <div
-          className={`col text-${data.position} custom-margin-${data.position}`}
-          data-aos={`zoom-in-${data.zoom}`}
-          data-aos-duration="800"
-        >
-          <h2 className="">{data.title}</h2>
-          <a href={data.deployed} className="">
-            <p>application</p>
+    // <div
+    //   className=" row full-height"
+    //   data-aos="fade-up"
+    //   data-aos-duration="700"
+    //   id="projects"
+    // >
+    //   <div className="col-sm-12 custom-margin-top">
+    //     <div
+    //       className={`col text-${data.position} custom-margin-${data.position}`}
+    //       data-aos={`zoom-in-${data.zoom}`}
+    //       data-aos-duration="800"
+    //     >
+    //       <h2 className="">{data.title}</h2>
+    //       <a href={data.deployed} className="">
+    //         <p>application</p>
+    //       </a>
+    //       <a href={data.repo} className="">
+    //         <p>repository</p>
+    //       </a>
+    //     </div>
+    //     <div className="container">
+    //       <figure
+    //         className=""
+    //         data-aos={`zoom-in-${data.position}`}
+    //         data-aos-duration="800"
+    //       >
+    //         <img
+    //           className="img-fluid rounded"
+    //           src={imgCMS[data.img]}
+    //           alt="project screenshot"
+    //         />
+    //       </figure>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="col-sm-4">
+      <div className="card" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h3 className="card-title">{data.title}</h3>
+          <a className="card-title" href={data.deployed}>
+            Visit
           </a>
-          <a href={data.repo} className="">
-            <p>repository</p>
+          <a className="card-title" href={data.repo}>
+            GitHub
           </a>
-        </div>
-        <div className="container">
-          <figure
-            className=""
-            data-aos={`zoom-in-${data.position}`}
-            data-aos-duration="800"
-          >
-            <img
-              className="img-fluid rounded"
-              src={imgCMS[data.img]}
-              alt="project screenshot"
-            />
-          </figure>
         </div>
       </div>
     </div>
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,24 +3,21 @@ import React from "react";
 
 // components
 import Project from "../components/project";
+import Header from "../components/header";
 
 // utils
 import projects from "../utils/projects.json";
 
 function Portfolio() {
   return (
-    // <div id="projects" className="full-height" style={{backgroundColor: "lightgray", overflow: "hidden"}}>
-    //   <div className="full-height container">
-    //     {projects.map(item => (
-    //       <Project data={item} />
-    //     ))}
-    //   </div>
-    // </div>
-    <>
-      {projects.map(item => (
-        <Project data={item} />
-      ))}
-    </>
+    <div className="full-height" id="projects">
+      <Header />
+      <div className="row">
+        {projects.map((item) => (
+          <Project data={item} />
+        ))}
+      </div>
+    </div>
   );
 }
 
