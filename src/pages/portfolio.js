8c497c0991ad59af3c8645bd2cf65e94b8aa1a47
Jay Kim <jaykim92@gmail.com>
2021-01-29 18:11:11 -0800

    updates to carousel styling

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,7 +6,7 @@ import { Carousel, Container } from "react-bootstrap";
 
 // components
 // import Project from "../components/project";
-// import Header from "../components/header";
+import Header from "../components/header";
 
 // utils
 // import projects from "../utils/projects.json";
@@ -28,33 +28,36 @@ function Portfolio() {
     //     </div>
     //   </div>
     // </div>
-    <div className="full-height ">
-      <Container className="carousel-height">
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/150"
-              alt="first project slide"
-            />
-            <Carousel.Caption>
-              <h3>Project 1</h3>
-              <p>Description of first project</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://via.placeholder.com/100"
-              alt="second project slide"
-            />
-            <Carousel.Caption>
-              <h3>Project 2</h3>
-              <p>Description of second project</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </Container>
+    <div className="full-height projects-img">
+      <Header />
+      <div className="carousel-margin-top">
+        <Container className="carousel-height">
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/150"
+                alt="first project slide"
+              />
+              <Carousel.Caption>
+                <h3>Project 1</h3>
+                <p>Description of first project</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/100"
+                alt="second project slide"
+              />
+              <Carousel.Caption>
+                <h3>Project 2</h3>
+                <p>Description of second project</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Container>
+      </div>
     </div>
   );
 }
